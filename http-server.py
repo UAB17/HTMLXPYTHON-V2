de75c42b556500d4e0290349f578cceed6b80d43
UAB17 <122218632+UAB17@users.noreply.github.com>
2023-01-08 15:09:02 +0000

    http-server.py
    A update, and I edited all error's.

--- http-server.py
+++ http-server.py
@@ -1,21 +1,18 @@
-# Coding:utf-8
-
-import http.server
+import http.server as http
 import socketserver
 from urllib.error import HTTPError
 import urllib
-
 
 port = 80
 adress = ("", port)
 
 handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(adress, handler)
+httpd = socketserver.TCPServer(adress, handler, bind_and_activate=False)
+httpd.allow_reuse_address = True
+httpd.server_bind()
+httpd.server_activate()
 
-print(f"Server is started in the PORT {port}")
-
-print("Go in your navigator and go this is link : localhost/")
+print("Server is started in the PORT {}".format(port))
+print("Go in your navigator and go to this link : localhost:{}/".format(port))
 
 httpd.serve_forever()
-
-
